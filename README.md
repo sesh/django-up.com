b7a34f7e68b22efd97c3f33753d6359e4cb91ddf
Brenton Cleeland <brenton@brntn.me>
2022-12-29 08:00:03 +1100

    feat: django-up.com base project

---
+++ README.md
@@ -0,0 +1,3 @@
+Sample project for [django-up](https://github.com/sesh/django-up).
+
+Deploys with Github Actions with `DJANGO_SECRET_KEY` and `UP_EMAIL` environment variables setup as Github Secrets.
---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+mkdir -p up && curl -L https://github.com/sesh/django-up/archive/refs/heads/main.zip | tar -x --strip-components=1 -C up
+
+pipenv install
+pipenv run pip freeze > requirements.txt
+
+pipenv run python manage.py up django-up.com
+
+rm -rf up
